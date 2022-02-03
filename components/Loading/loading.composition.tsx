4c9ae050b9c12a9a1cbec3c99e1d9bf8dae4b717
David Cohen <david.cohen@cbinsights.com>
2022-02-03 10:28:37 -0500

    loading component

--- components/Loading/loading.composition.tsx
+++ components/Loading/loading.composition.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Loading } from './loading';
 
+export const ExtraSmallLoading = () => <Loading size="xs" dataTest={''} />;
+
 export const SmallLoading = () => <Loading size="s" dataTest={''} />;
 
 export const MediumLoading = () => <Loading size="m" dataTest={''} />;
